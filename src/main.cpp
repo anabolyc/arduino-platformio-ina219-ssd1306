fde2a662b565b05ce0abd91cce0d7ee7c2f2f4b5
andrey.malyshenko <andrey.malyshenko@gmail.com>
2018-07-24 20:29:44 +0200

    Builds for Atmel 328p, fails for Attiny because of lack of both RAM and FLASH

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,96 @@
 #define SERIAL_SPEED 57600
+#define OLED_RESET 4
 
 #include "main.h"
-
-#define OLED_RESET 4
 Adafruit_SSD1306* display = new Adafruit_SSD1306(OLED_RESET);
-Adafruit_INA219* ina219 = new Adafruit_INA219();
 
 void setup(void)
 {
+  #if !_NO_SERIAL_
   Serial.begin(SERIAL_SPEED);
   while (!Serial) {
-     // will pause Zero, Leonardo, etc until serial console opens
     delay(1);
   }       
+  #endif 
 
-  // Initialize the ina219->
-  // By default the initialization will use the largest range (32V, 2A).  However
-  // you can call a setCalibration function to change this range (see comments).
-  ina219->begin();
-  // To use a slightly lower 32V, 1A range (higher precision on amps):
-  //ina219->setCalibration_32V_1A();
-  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
-  ina219->setCalibration_16V_400mA();
-
-  display->begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  // Clear the buffer.
-  display->clearDisplay();
-  display->display();
-
-  display->setTextSize(1);
-  display->setTextColor(WHITE);
+  display_init(display);
 }
 
 void loop(void)
 {
-  float shuntvoltage = 0;
-  float busvoltage = 0;
-  float current_mA = 0;
-  float loadvoltage = 0;
-  float power_mW = 0;
+  Adafruit_INA219* ina219 = new Adafruit_INA219();
+  ina_init(ina219);
+  ina_data* data = new ina_data();
+  get_ina_data(ina219, data);
+  delete ina219;
 
-  shuntvoltage = ina219->getShuntVoltage_mV();
-  busvoltage = ina219->getBusVoltage_V();
-  current_mA = ina219->getCurrent_mA();
-  power_mW = ina219->getPower_mW();
-  loadvoltage = busvoltage + shuntvoltage / 1000;
-
-  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
-  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
-  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
-  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
-  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+  #if !_NO_SERIAL_
+  Serial.print(F("Bus Voltage:   ")); Serial.print(data->busvoltage); Serial.println(F(" V"));
+  Serial.print(F("Shunt Voltage: ")); Serial.print(data->shuntvoltage); Serial.println(F(" mV"));
+  Serial.print(F("Load Voltage:  ")); Serial.print(data->loadvoltage); Serial.println(F(" V"));
+  Serial.print(F("Current:       ")); Serial.print(data->current_mA); Serial.println(F(" mA"));
+  Serial.print(F("Power:         ")); Serial.print(data->power_mW); Serial.println(F(" mW"));
   Serial.println("");
+  #endif
 
   display->clearDisplay();
 
   display->setCursor(0, 0);
-  display->print("Vb:");
-  display->print(busvoltage);
-  display->println(" V");
+  display->print(F("Vb:"));
+  display->print(data->busvoltage);
+  display->println(F(" V"));
 
   display->setCursor(0, 10);
-  display->print("Vs:");
-  display->print(shuntvoltage);
-  display->println("mV");
+  display->print(F("Vs:"));
+  display->print(data->shuntvoltage);
+  display->println(F("mV"));
 
   display->setCursor(0, 20);
-  display->print("Vl:");
-  display->print(loadvoltage);
-  display->println(" V");
+  display->print(F("Vl:"));
+  display->print(data->loadvoltage);
+  display->println(F(" V"));
 
   display->setCursor(64, 0);
-  display->print("I:");
-  display->print(current_mA);
-  display->println(" mA");
+  display->print(F("I:"));
+  display->print(data->current_mA);
+  display->println(F(" mA"));
 
   display->setCursor(64, 10);
-  display->print("P:");
-  display->print(power_mW);
-  display->println(" mW");
+  display->print(F("P:"));
+  display->print(data->power_mW);
+  display->println(F(" mW"));
 
-  display->display();
+  display->display(); 
+
+  delete data;
   delay(50);
+}
+
+void ina_init(Adafruit_INA219* self) {
+  // Initialize the ina219->
+  // By default the initialization will use the largest range (32V, 2A).  However
+  // you can call a setCalibration function to change this range (see comments).
+  self->begin();
+  // To use a slightly lower 32V, 1A range (higher precision on amps):
+  //ina219->setCalibration_32V_1A();
+  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
+  self->setCalibration_16V_400mA();
+}
+
+void get_ina_data(Adafruit_INA219* self, ina_data* data) {
+  data->shuntvoltage = self->getShuntVoltage_mV();
+  data->busvoltage = self->getBusVoltage_V();
+  data->current_mA = self->getCurrent_mA();
+  data->power_mW = self->getPower_mW();
+  data->loadvoltage = data->busvoltage + data->shuntvoltage / 1000;
+}
+
+void display_init(Adafruit_SSD1306* self) {
+  self->begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  // Clear the buffer.
+  //self->clearDisplay();
+  //sswordself->display();
+
+  self->setTextSize(1);
+  self->setTextColor(WHITE);
 }
--- src/main.h
+++ src/main.h
@@ -1,3 +1,21 @@
-#include <Adafruit_GFX.h>
+#ifndef _MAIN_H
+#define _MAIN_H
+
+
 #include "Adafruit_SSD1306.h"
 #include "Adafruit_INA219.h"
+
+struct ina_data {
+    float shuntvoltage;
+    float busvoltage;
+    float current_mA;
+    float loadvoltage;
+    float power_mW;
+};
+
+
+void ina_init(Adafruit_INA219* self);
+void display_init(Adafruit_SSD1306* self);
+void get_ina_data(Adafruit_INA219* self, ina_data* data);
+
+#endif
